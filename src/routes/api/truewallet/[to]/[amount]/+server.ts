697d7eb587e34d4d3efb51af388111f3e64add14
chanios <chanios@proton.me>
2024-03-31 16:36:48 +0700

    update

---
+++ src/routes/api/truewallet/[to]/[amount]/+server.ts
@@ -0,0 +1,22 @@
+import { generate } from 'promptparse'
+import qrcode from 'qrcode';
+
+/** @type {import('./$types').RequestHandler} */
+// @ts-ignore
+export async function GET({ url, params }) {
+    const { message } = url.searchParams.get('message')||'';
+
+    const payload = generate.truemoney({
+        mobileNo: params.to,
+        amount: +params.amount,
+        message: message
+    })
+
+    const buf = await qrcode.toBuffer(payload);
+
+    return new Response(buf, {
+        headers: {
+            'Content-Type': 'image/png'
+        }
+    });
+}
